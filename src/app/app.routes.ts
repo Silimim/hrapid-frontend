f6d7c60872fce4401ddcb9388a2b2cd638db904b
Simone Baptiste <baptistesimone19@gmail.com>
2024-11-05 23:57:14 +0100

    base route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {authGuard} from './inteceptors/auth.guard';
+import {LoginComponent} from './components/login/login.component';
+import {LayoutComponent} from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    title: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [authGuard],
+  }
+];
