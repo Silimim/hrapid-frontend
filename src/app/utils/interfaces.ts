1099003fabf197f51ce1439355c33c1a3bd18aa7
Simone Baptiste <baptistesimone19@gmail.com>
2024-11-11 21:47:11 +0100

    interfaces

--- src/app/utils/interfaces.ts
+++ src/app/utils/interfaces.ts
@@ -5,12 +5,65 @@ export enum Role {
 }
 
 export interface User {
+  id: number;
+  name: string;
+  last_name: string | null;
   username: string;
+  password: string | null;
   email: string;
+  phone: string | null;
   roles: Role[];
 }
 
 export interface TokensPair {
   access_token: string;
   refresh_token: string;
+}
+
+export interface Company {
+  id: number;
+  company_name: string;
+  address: string | null;
+  city: string | null;
+  province: string | null;
+  country: string | null;
+  phone1: string;
+  phone2: string | null;
+  email1: string;
+  email2: string | null;
+  sales: number | null;
+  date_added: string | null;
+  user_added_id: number | null;
+}
+
+export interface Employee {
+  id: number;
+  name: string;
+  last_name: string;
+  phone1: string | null;
+  phone2: string | null;
+  email1: string | null;
+  email2: string | null;
+  role: string | null;
+  date_added: string | null;
+  user_added_id: number | null;
+}
+
+export interface List {
+  id: number;
+  name: string;
+  description: string | null;
+  date_added: string | null;
+  user_added_id: number | null;
+}
+
+export interface AutoTable {
+  data: any[];
+  headers: AutoTableHeaders[];
+}
+
+export interface AutoTableHeaders {
+  header: string;
+  field: string;
+  type: string;
 }
