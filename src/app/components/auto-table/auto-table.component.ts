81681da6414bb9d4809ecbeacd141f3379ee93c4
Simone Baptiste <baptistesimone19@gmail.com>
2024-11-12 17:11:54 +0100

    implemented formatters

--- src/app/components/auto-table/auto-table.component.ts
+++ src/app/components/auto-table/auto-table.component.ts
@@ -1,14 +1,27 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {TableModule} from 'primeng/table';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Table, TableModule} from 'primeng/table';
 import {HttpClient} from '@angular/common/http';
 import {MessageService} from 'primeng/api';
 import {AutoTable, AutoTableHeaders} from '../../utils/interfaces';
+import {CurrencyPipe, DatePipe} from '@angular/common';
+import {Button} from 'primeng/button';
+import {IconFieldModule} from 'primeng/iconfield';
+import {DropdownModule} from 'primeng/dropdown';
+import {TagModule} from 'primeng/tag';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'auto-table',
   standalone: true,
   imports: [
-    TableModule
+    TableModule,
+    CurrencyPipe,
+    DatePipe,
+    Button,
+    IconFieldModule,
+    DropdownModule,
+    TagModule,
+    FormsModule
   ],
   templateUrl: './auto-table.component.html',
   styleUrl: './auto-table.component.css'
@@ -17,9 +30,13 @@ export class AutoTableComponent implements OnInit {
 
   @Input() tableName: string | undefined;
 
+  @ViewChild('dt') table: Table | undefined;
+
   tableData: any[] | undefined;
 
   tableColumns: AutoTableHeaders[] | undefined;
+
+  loading: boolean = true;
 
   constructor(private http: HttpClient, private messageService: MessageService) {
   }
@@ -34,9 +51,11 @@ export class AutoTableComponent implements OnInit {
             return {
               field: header.field,
               header: header.header,
-              type: this.getColumnType(header.type)
+              type: this.getColumnType(header.type),
+              formatType: header.formatType
             };
           });
+          this.loading = false;
         },
         error: (error: any) => {
           this.messageService.add({severity: 'error', summary: 'Error', detail: 'Error loading table data'});
@@ -64,5 +83,14 @@ export class AutoTableComponent implements OnInit {
       default:
         return 'text';
     }
+  }
+
+  getSeverity(status: string, enumType: { key: string, value: any }[]): any {
+    const found = enumType.find((item) => item.key === status);
+    return found ? found.value : 'info';
+  }
+
+  addRow() {
+
   }
 }
