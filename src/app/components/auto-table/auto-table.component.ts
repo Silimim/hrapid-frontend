27a60a4d482036137cd6d2afd3c96a036251f29e
Simone Baptiste <baptistesimone19@gmail.com>
2024-12-03 17:11:55 +0100

    code refactoring, added session storage for table state, added actions

--- src/app/components/auto-table/auto-table.component.ts
+++ src/app/components/auto-table/auto-table.component.ts
@@ -1,9 +1,9 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import {Table, TableModule} from 'primeng/table';
+import {Table, TableModule, TableRowSelectEvent} from 'primeng/table';
 import {HttpClient} from '@angular/common/http';
 import {MessageService} from 'primeng/api';
 import {AutoFormMode, AutoTable, AutoTableHeaders, CrudPaths} from '../../utils/interfaces';
-import {CurrencyPipe, DatePipe} from '@angular/common';
+import {CurrencyPipe, DatePipe, NgIf} from '@angular/common';
 import {Button} from 'primeng/button';
 import {IconFieldModule} from 'primeng/iconfield';
 import {DropdownModule} from 'primeng/dropdown';
@@ -11,6 +11,7 @@ import {TagModule} from 'primeng/tag';
 import {FormsModule} from '@angular/forms';
 import {DialogModule} from 'primeng/dialog';
 import {AutoFormComponent} from '../auto-form/auto-form.component';
+import {MultiSelect, MultiSelectModule} from 'primeng/multiselect';
 
 @Component({
   selector: 'auto-table',
@@ -25,7 +26,9 @@ import {AutoFormComponent} from '../auto-form/auto-form.component';
     TagModule,
     FormsModule,
     DialogModule,
-    AutoFormComponent
+    AutoFormComponent,
+    MultiSelectModule,
+    NgIf
   ],
   templateUrl: './auto-table.component.html',
   styleUrl: './auto-table.component.css'
@@ -38,12 +41,20 @@ export class AutoTableComponent implements OnInit {
 
   tableData: any[] | undefined;
 
-  tableColumns: AutoTableHeaders[] | undefined;
+  rowData: any | undefined;
+
+  tableColumns!: AutoTableHeaders[];
+
+  selectedTableColumns!: AutoTableHeaders[];
 
   loading: boolean = true;
 
   showModal: boolean = false;
   modalMode: AutoFormMode = AutoFormMode.Add;
+
+  refreshed: boolean = true;
+
+  detail: any;
 
   constructor(private http: HttpClient, private messageService: MessageService) {
   }
@@ -51,25 +62,7 @@ export class AutoTableComponent implements OnInit {
   ngOnInit() {
 
     if (this.crudPaths?.get) {
-      this.http.get<AutoTable>(`http://localhost:8080/table/${this.crudPaths?.get}`).subscribe({
-        next: (data) => {
-          this.tableData = data.data.length ? data.data : [];
-          this.tableColumns = data.headers.map((header) => {
-            return {
-              field: header.field,
-              header: header.header,
-              type: this.getColumnType(header.type),
-              formatType: header.formatType,
-              inputType: header.inputType,
-              required: header.required
-            };
-          });
-          this.loading = false;
-        },
-        error: (error: any) => {
-          this.messageService.add({severity: 'error', summary: 'Error', detail: 'Error loading table data'});
-        }
-      })
+      this.getData();
     }
   }
 
@@ -94,6 +87,29 @@ export class AutoTableComponent implements OnInit {
     }
   }
 
+  getData() {
+    this.http.get<AutoTable>(`http://localhost:8080/table/${this.crudPaths?.get}`).subscribe({
+      next: (data) => {
+        this.tableData = data.data.length ? data.data : [];
+        this.tableColumns = data.headers.map((header) => {
+          return {
+            field: header.field,
+            header: header.header,
+            type: this.getColumnType(header.type),
+            formatType: header.formatType,
+            inputType: header.inputType,
+            required: header.required
+          };
+        });
+        this.selectedTableColumns = this.tableColumns;
+        this.loading = false;
+      },
+      error: () => {
+        this.messageService.add({severity: 'error', summary: 'Error', detail: 'Error loading table data'});
+      }
+    })
+  }
+
   getSeverity(status: string, enumType: { key: string, value: any }[]): any {
     const found = enumType.find((item) => item.key === status);
     return found ? found.value : 'info';
@@ -102,5 +118,40 @@ export class AutoTableComponent implements OnInit {
   addRow() {
     this.modalMode = AutoFormMode.Add;
     this.showModal = true
+  }
+
+  editRow(data: any) {
+    this.rowData = data;
+    this.modalMode = AutoFormMode.Edit;
+    this.showModal = true;
+  }
+
+  deleteRow(data: any) {
+    this.http.delete(`http://localhost:8080/api/${this.crudPaths?.delete}/${data.id}`).subscribe({
+      next: () => {
+        this.refreshData(true);
+        this.messageService.add({severity: 'success', summary: 'Success', detail: 'Row deleted successfully'});
+      },
+      error: () => {
+        this.messageService.add({severity: 'error', summary: 'Error', detail: 'Error deleting row'});
+      }
+    });
+  }
+
+  refreshData(event: any) {
+    if (event) {
+      this.getData();
+    }
+  }
+
+  showModalChange(show: boolean) {
+    if (!show) {
+      this.showModal = show;
+      this.rowData = undefined;
+    }
+  }
+
+  openDetail(data: {detail: string, [key: string]: string}) {
+
   }
 }
